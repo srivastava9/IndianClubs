ad91c6af827c5bcffebab01171710f443b3e7cd5
srivastava9 <krishu4626@gmail.com>
2020-04-28 20:53:01 +0530

    made club and state models and migrations for them

--- Club/admin.py
+++ Club/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Club, State
 # Register your models here.
+
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+admin.site.register(State, StateAdmin)
+
+
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("Name", "League", "State", "City")
+    search_fields = ("State", "City")
+    list_filter = ("State", "League", "City")
+
+
+admin.site.register(Club, ClubAdmin)
--- Club/models.py
+++ Club/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class State(models.Model):
+    name = models.CharField(max_length=255, verbose_name="State")
+
+    def __str__(self):
+        return self.name
+
+
+# class League(models.Model):
+#     name = models.CharField(max_length=255, verbose_name="League")
+
+#     def __str__(self):
+#         return self.name
+
+
+class Club(models.Model):
+    Name = models.CharField(
+        max_length=200, verbose_name="Club Name", unique=True)
+    State = models.OneToOneField(
+        to=State, on_delete=models.CASCADE, related_name="clubs")
+    League = models.CharField(
+        max_length=200, verbose_name="League Name")
+    City = models.CharField(max_length=200, verbose_name="City")
+
+    def __str__(self):
+        return f'{self.Name}-{self.State}'
+
+    class Meta:
+        verbose_name_plural = "Clubs"
